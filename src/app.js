193b06122780c598df65430c6da21258190c956b
Akeef M Choudhury <91553592+akeefmc@users.noreply.github.com>
2022-07-04 16:13:11 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+if (process.env.USER) require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors")
+
+// Routers Import
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+
+// Errors Import
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+// But I know this one!
+app.use(cors())
+
+// Still don't know what this does
+app.use(express.json());
+
+// Routers
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
+// Errors
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
